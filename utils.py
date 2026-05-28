ec4d2f6cb76c39e86984b502dfea4a65e9f49d44
antwie04 <antoni1404@interia.pl>
2026-05-28 18:21:27 +0200

    sadf

--- utils.py
+++ utils.py
@@ -1,14 +1,18 @@
+"""methods for calculator"""
 def add(a: int, b: int) -> int:
+    """method for adding two numbers"""
     return a + b
 
 
 def subtract(a: int, b: int) -> int:
+    """method for substracting two numbers"""
     return a - b
 
 
 def multiply(a: int, b: int) -> int:
+    """method for multiplying two numbers"""
     return a * b
 
-
 def divide(a: int, b: int) -> float:
+    """method for dividing two numbers"""
     return a / b
